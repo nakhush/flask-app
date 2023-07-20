504a7c8debdc6a239feaa37eb561110b8c895971
nakhush <mnakhushev1@gmail.com>
2023-07-20 18:42:28 +0300

    working app

--- app.py
+++ app.py
@@ -5,3 +5,11 @@ application = Flask(__name__)
 @application.route("/")
 def home():
     return render_template("index.html")
+
+@application.route("/info")
+def info():
+    return render_template("info.html")
+
+@application.route("/owner")
+def owner():
+    return render_template("owner.html")
